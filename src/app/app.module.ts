62acaa7ce44a6b59aa122506ca872a7689481e0b
Nice <nicenicole.in@gmail.com>
2019-08-29 14:51:56 +0200

    created user component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GitFormComponent } from './git-form/git-form.component';
 import { TimeCountPipe } from './time-count.pipe';
+import { HttpClientModule} from '@angular/common/http'
+import { FormsModule} from '@angular/forms';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GitFormComponent,
-    TimeCountPipe
+    TimeCountPipe,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
