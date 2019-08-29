625e062cc0ebd4aea8413edf2955dd89e9c3af40
Nice <nicenicole.in@gmail.com>
2019-08-29 14:09:09 +0200

    added a form

--- src/app/repository.ts
+++ src/app/repository.ts
@@ -1,2 +1,3 @@
 export class Repository {
+    constructor(public repoName:string, public description:string, public date:Date){}
 }
--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,6 @@
 export class User {
+    // public showListRepo:boolean;
+    constructor(public username: string, public profilePhoto:string, public nberRepo:number){
+        // this.showListRepo=false;
+    }
 }
